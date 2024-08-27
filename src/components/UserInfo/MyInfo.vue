439f7b27bfa5adcb7442aba04069dfa4ebb68e21
zhangxu <3191240387@qq.com>
2024-08-27 15:18:07 +0800

    updata_zx01

--- src/components/UserInfo/MyInfo.vue
+++ src/components/UserInfo/MyInfo.vue
@@ -3,8 +3,14 @@
     <div class="userContainer">
       <div class="avatarContainer">
         <el-avatar :src="avatar" :size="170"></el-avatar>
-        <div class="signature"><span>个性签名：</span>
-          {{ userInfoForm.signature }}</div> <!-- 个性签名展示 -->
+        <div class="signature"><span>个性签名：</span>{{ userInfoForm.signature }}</div>
+        <el-upload class="avatar-uploader" :action="$apiServer + 'upload?type=1'" :show-file-list="false"
+          :on-success="handleAvatarSuccess" :before-upload="beforeuploadButton" :limit="1">
+          <img v-if="isUpload" :src="preAvatar" class="avatarImg" />
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+        <el-button type="primary" class="uploadButton" @click="uploadAvatar">更换头像</el-button>
+
       </div>
       <div class="userInfoContainer">
         <el-form label-position="top" :model="userInfoForm" ref="userInfoFormRef">
@@ -12,8 +18,7 @@
             <el-input v-model="userInfoForm.username" :disabled="!isEditing"></el-input>
           </el-form-item>
           <el-form-item label="个性签名">
-            <el-input v-model="userInfoForm.signature" prefix-icon="iconfont icon-qianming"
-              :disabled="!isEditing"></el-input>
+            <el-input v-model="userInfoForm.signature" :disabled="!isEditing"></el-input>
           </el-form-item>
           <el-form-item label="性别">
             <el-radio :label="1" v-model="userInfoForm.sex" :disabled="!isEditing">男</el-radio>
@@ -25,14 +30,10 @@
           </el-form-item>
         </el-form>
         <div class="buttonGroup">
-          <el-button type="primary" class="uploadButton" @click="triggerUpload">
-            更新头像
-          </el-button>
           <el-button type="primary" class="userInfoButton" @click="toggleEdit">
             {{ isEditing ? '保存' : '修改编辑' }}
           </el-button>
         </div>
-        <input ref="fileInput" type="file" accept="image/*" style="display: none" @change="handleFileChange" />
       </div>
     </div>
   </div>
@@ -46,8 +47,8 @@ export default {
       preAvatar: "",
       isUpload: false,
       tempUrl: "",
+      isEditing: false,
       userInfoForm: {},
-      isEditing: false, // 是否处于编辑状态
     };
   },
   mounted() {
@@ -57,43 +58,44 @@ export default {
     this.userInfoForm = userInfo;
   },
   methods: {
-    triggerUpload() {
-      this.$refs.fileInput.click(); // 触发文件选择框
-    },
-    handleFileChange(event) {
-      const file = event.target.files[0];
-      if (!file) return;
-
+    beforeuploadButton(file) {
       const isJPG1 = file.type === "image/jpeg";
       const isJPG2 = file.type === "image/jpg";
       const isJPG3 = file.type === "image/png";
       const isLt2M = file.size / 1024 / 1024 < 2;
       if (!isJPG1 && !isJPG2 && !isJPG3) {
         this.$message.error("上传头像图片只能是 JPG 或 PNG 格式!");
-        return;
       }
       if (!isLt2M) {
         this.$message.error("上传头像图片大小不能超过 2MB!");
-        return;
       }
-
-      this.uploadAvatar(file);
+      return (isJPG1 || isJPG2 || isJPG3) && isLt2M;
     },
-    async uploadAvatar(file) {
-      const formData = new FormData();
-      formData.append('file', file);
-
-      const { data: res } = await this.$http.post(this.$apiServer + 'upload?type=1', formData);
-
+    handleAvatarSuccess(res, file) {
+      this.isUpload = true;
+      this.preAvatar = URL.createObjectURL(file.raw);
+      this.tempUrl = res;
+    },
+    async uploadAvatar() {
+      if (!this.isUpload) {
+        return this.$message.warning("请先选择要更换的头像!");
+      }
+      const { data: res } = await this.$http.post("user/update/avatar", {
+        id: this.userInfoForm.id,
+        imgUrl: this.tempUrl,
+      });
       if (res.code !== 200) {
         return this.$message.error(res.msg);
       }
-
       this.$message.success(res.msg);
       this.avatar = res.url;
+      this.isUpload = false;
       this.userInfoForm.avatar = this.avatar;
       this.$store.commit("setAvatar", this.avatar);
-      window.sessionStorage.setItem("userInfo", JSON.stringify(this.userInfoForm));
+      window.sessionStorage.setItem(
+        "userInfo",
+        JSON.stringify(this.userInfoForm)
+      );
     },
     toggleEdit() {
       if (this.isEditing) {
@@ -104,55 +106,67 @@ export default {
     updateUserInfo() {
       this.$refs.userInfoFormRef.validate(async (valid) => {
         if (!valid) return;
-        const { data: res } = await this.$http.post("user/update/userinfo", {
+
+        const { data: res } = await this.$http.post(this.$apiServer + "user/update/userinfo", {
           id: this.userInfoForm.id,
           signature: this.userInfoForm.signature,
           sex: this.userInfoForm.sex,
           birthday: this.userInfoForm.birthday,
         });
+
         if (res.code !== 200) {
           return this.$message.error(res.msg);
         }
+
         this.$message.success(res.msg);
-        window.sessionStorage.setItem(
-          "userInfo",
-          JSON.stringify(this.userInfoForm)
-        );
+
+        // 更新 sessionStorage 中的用户信息
+        window.sessionStorage.setItem("userInfo", JSON.stringify(this.userInfoForm));
       });
     },
   },
 };
 </script>
-
 <style lang="less" scoped>
 .userContainer {
   display: flex;
   justify-content: space-around;
 }
-
-.avatarContainer {
-  width: 170px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-      margin: 80px;
-}
-
 .el-avatar {
   margin: 0 auto;
   margin: 40px 0;
 }
 
-.signature {
-  margin-bottom: 20px;
-  font-size: 16px;
-  color: #666;
+.avatar-uploader {
+  border: 1px dashed rgb(206, 206, 206);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+
+.avatar-uploader:hover {
+  border-color: #409eff;
+}
+
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 170px;
+  height: 170px;
+  line-height: 170px;
   text-align: center;
-  font-weight: 500;
-  span{
-  font-size: 16px;
-  font-weight: 700;
-  }
+}
+
+.avatarImg {
+  width: 170px;
+  height: 170px;
+  display: block;
+}
+
+.uploadButton {
+  text-align: center;
+  margin: 30px auto;
 }
 
 .userInfoContainer {
@@ -160,15 +174,38 @@ export default {
   margin: 30px 120px;
 }
 
-.buttonGroup {
-    display: flex;
-      justify-content: center;
-      margin-top: 20px;
-      align-items: center;
+
+.avatarContainer {
+  width: 170px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 100px;
 }
 
-.uploadButton,
+.signature {
+  margin-bottom: 20px;
+  font-size: 14px;
+  color: #666;
+  text-align: center;
+  font-weight: 500;
+
+  span {
+    font-size: 16px;
+    font-weight: 700;
+  }
+}
+
+
+.buttonGroup {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  align-items: center;
+}
+
+
 .userInfoButton {
-  width: 18%;
+  // width: 18%;
 }
 </style>
